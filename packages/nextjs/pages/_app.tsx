608b445c3dcda27a354ddac5996203d5f8401190
xritzx <ritankarpaul47@gmail.com>
2023-12-09 00:19:24 +0530

    feat: add waku context

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -1,36 +1,24 @@
-import { useEffect, useState } from "react";
-import type { AppProps } from "next/app";
-import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
-import "@rainbow-me/rainbowkit/styles.css";
-import NextNProgress from "nextjs-progressbar";
-import { Toaster } from "react-hot-toast";
-import { useDarkMode } from "usehooks-ts";
-import { WagmiConfig } from "wagmi";
-import { Footer } from "~~/components/Footer";
-import { Header } from "~~/components/Header";
-import { BlockieAvatar } from "~~/components/scaffold-eth";
-import { wagmiConfig } from "~~/services/web3/wagmiConfig";
-import { appChains } from "~~/services/web3/wagmiConnectors";
-import "~~/styles/globals.css";
+import type { AppProps } from 'next/app';
+import { RainbowKitProvider, darkTheme, lightTheme } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+import NextNProgress from 'nextjs-progressbar';
+import { Toaster } from 'react-hot-toast';
+import { WagmiConfig } from 'wagmi';
+import { Footer } from '~~/components/Footer';
+import { Header } from '~~/components/Header';
+import { BlockieAvatar } from '~~/components/scaffold-eth';
+import { LightNodeProvider } from '~~/services/waku/LightNodeContext';
+import { wagmiConfig } from '~~/services/web3/wagmiConfig';
+import { appChains } from '~~/services/web3/wagmiConnectors';
+import '~~/styles/globals.css';
+import { useDarkMode } from 'usehooks-ts';
+import { useEffect, useState } from 'react';
+
 
 const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="relative flex flex-col flex-1">
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-      </div>
-      <Toaster />
-    </>
-  );
-};
-
-const ScaffoldEthAppWithProviders = (props: AppProps) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
   const { isDarkMode } = useDarkMode();
+
   useEffect(() => {
     setIsDarkTheme(isDarkMode);
   }, [isDarkMode]);
@@ -43,10 +31,19 @@ const ScaffoldEthAppWithProviders = (props: AppProps) => {
         avatar={BlockieAvatar}
         theme={isDarkTheme ? darkTheme() : lightTheme()}
       >
-        <ScaffoldEthApp {...props} />
+        <LightNodeProvider>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="relative flex flex-col flex-1">
+              <Component {...pageProps} />
+            </main>
+            <Footer />
+          </div>
+        </LightNodeProvider>
+        <Toaster />
       </RainbowKitProvider>
     </WagmiConfig>
   );
 };
 
-export default ScaffoldEthAppWithProviders;
+export default ScaffoldEthApp;
