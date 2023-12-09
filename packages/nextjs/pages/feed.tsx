dafffe50cd8a7151fea996293fabc816d8ce391c
Sumit Guha <sumit03guha@gmail.com>
2023-12-09 11:56:42 +0530

    added verifier server

--- packages/nextjs/pages/feed.tsx
+++ packages/nextjs/pages/feed.tsx
@@ -38,7 +38,7 @@ const Feed = () => {
   const { accountAddress, category } = useGlobalState();
   const [uploads, setUploads] = useState<any>({});
   const [date] = useState(format(new Date(), 'yyyy-MM-dd'));
-
+  const [provedAccess, setProvedAccess] = useState(false);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   const openModal = () => setModalOpen(true);
@@ -201,7 +201,54 @@ const Feed = () => {
   }
 
   return (
-    <>
+    <>{!provedAccess ? (
+      <Center className={styles['vc-check-page']}>
+        <Container size={'1px'}>
+          <ChakraCarda
+            style={{
+              border: '2px solid #805AD5',
+            }}
+          >
+            <CardBody style={{ paddingBottom: 0 }}>
+              <p>
+                This is a fullstack template for creating a Polygon ID VC{' '}
+                <a href="https://0xpolygonid.github.io/tutorials/#core-concepts-of-polygon-id-verifiable-credentials-identity-holder-issuer-and-verifier-triangle-of-trust">
+                  (Verifiable Credential)
+                </a>{' '}
+                gated dapp. Prove you were born before January 1, 2023 to use the dapp
+              </p>
+
+              <PolygonIDVerifier
+                publicServerURL={process.env.NEXT_PUBLIC_REACT_APP_VERIFICATION_SERVER_PUBLIC_URL as string}
+                localServerURL={process.env.NEXT_PUBLIC_REACT_APP_VERIFICATION_SERVER_LOCAL_HOST_URL as string}
+                credentialType={'KYCAgeCredential'}
+                issuerOrHowToLink={
+                  'https://oceans404.notion.site/How-to-get-a-Verifiable-Credential-f3d34e7c98ec4147b6b2fae79066c4f6?pvs=4'
+                }
+                onVerificationResult={setProvedAccess}
+              />
+              <Image
+                src="https://bafybeibcgo5anycve5flw6pcz5esiqkvrzlmwdr37wcqu33u63olskqkze.ipfs.nftstorage.link/"
+                alt="Polygon devs image"
+                borderRadius="lg"
+              />
+            </CardBody>
+            <a href="https://twitter.com/0ceans404" target="_blank" rel="noreferrer">
+              <p
+                style={{
+                  position: 'absolute',
+                  bottom: '-15px',
+                  right: '0',
+                  fontSize: '8px',
+                }}
+              >
+                Template built with 💜 by Steph
+              </p>
+            </a>
+          </ChakraCarda>
+        </Container>
+      </Center>
+    ) : (
       <Box className={commonStyles['page-container']}>
 
         <div className="p-4">
@@ -278,7 +325,8 @@ const Feed = () => {
         ))}
       </Box>
     </>
-  );
+  )
+              };
 };
 
 export default Feed;
